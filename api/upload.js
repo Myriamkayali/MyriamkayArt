aa607e00eb742795e0261b0be1db1a7059b9a17e
Myriamkayali <Myriamkayali@users.noreply.github.com>
2026-04-19 19:39:06 +0300

    update

--- api/upload.js
+++ api/upload.js
@@ -1,33 +1,17 @@
 const { put } = require('@vercel/blob');
-const multiparty = require('multiparty');
-const fs = require('fs');
 
 module.exports = async function handler(req, res) {
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
-  const form = new multiparty.Form();
+  const filename = decodeURIComponent(req.headers['x-filename'] || 'upload');
+  const contentType = req.headers['content-type'] || 'application/octet-stream';
 
-  const { fields, files } = await new Promise((resolve, reject) => {
-    form.parse(req, (err, fields, files) => {
-      if (err) reject(err);
-      else resolve({ fields, files });
-    });
-  });
-
-  const file = files?.file?.[0];
-  if (!file) {
-    return res.status(400).json({ error: 'No file provided' });
+  try {
+    const blob = await put(filename, req, { access: 'public', contentType });
+    return res.status(200).json({ url: blob.url });
+  } catch (e) {
+    return res.status(500).json({ error: e.message });
   }
-
-  const stream = fs.createReadStream(file.path);
-  const blob = await put(file.originalFilename || 'upload', stream, {
-    access: 'public',
-    contentType: file.headers['content-type'],
-  });
-
-  fs.unlink(file.path, () => {});
-
-  return res.status(200).json({ url: blob.url });
-}
+};
